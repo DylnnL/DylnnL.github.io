4529b5f40f06bf0658d2c6d14040ca97b7239ceb
DylnnL <dylanlim388@gmail.com>
2024-01-21 12:52:30 +0700

    first commit

---
+++ KANSO-master/scripts/login.js
@@ -0,0 +1,5 @@
+document
+  .querySelector(".js-login-button")
+  .addEventListener("click", function () {
+    window.location.href = "./index.html";
+  });
---
+++ KANSO-master/scripts/register.js
@@ -0,0 +1,5 @@
+document
+  .querySelector(".js-register-button")
+  .addEventListener("click", function () {
+    window.location.href = "./index.html";
+  });
